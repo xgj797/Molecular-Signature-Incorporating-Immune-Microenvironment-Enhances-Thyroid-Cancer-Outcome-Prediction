aff111ad4d86985b81761c0ec43b4a618a4b1f91
mloberg16 <80286246+mloberg16@users.noreply.github.com>
2022-10-24 11:50:10 -0400

    Create Sample7_Add_TDS_ModuleScore.R

---
+++ Visium_Analysis/Sample_7/Sample7_Add_TDS_ModuleScore.R
@@ -0,0 +1,33 @@
+# Author: Matthew Aaron Loberg
+# Date: 22-0913
+# Goal: read in pre-processed Sample7 and add TDS as a module score
+# Visiualize TDS spatially
+
+# Load packages:
+library(tidyverse)
+library(Seurat)
+
+# Load processed Sample7
+Sample7 <- readRDS(file = "Data_in_Use/Sample7/Sample7_Clustered_0.3Res.rds")
+
+### Add the TDS Score as a module score
+# Create a gene list for TDS (16 genes total)
+TDS_Genes <- c("DIO1", "DIO2", "DUOX1", "DUOX2", "FOXE1", 
+               "GLIS3", "NKX2-1", "PAX8", "SLC26A4", "SLC5A5", 
+               "SLC5A8", "TG", "THRA", "THRB", "TPO", "TSHR")
+
+# Turn into a list
+TDS_List <- list(TDS_Genes)
+rm(TDS_Genes) # clean up TDS_Genes
+
+# Add TDS score to Sample7                                
+Sample7 <- AddModuleScore(object = Sample7,
+                          features = TDS_List,
+                          assay = "SCT",
+                          name = 'TDS')
+
+# Print TDS Spatial Feature Plot
+TDS_Spatial <- SpatialFeaturePlot(Sample7, features = c("TDS1"))
+ggsave("outputs/Sample7_ModuleScores/22-0913_Sample7_TDS_ModuleScore.png",
+       TDS_Spatial,
+       width = 4, height = 5, dpi = 600)
