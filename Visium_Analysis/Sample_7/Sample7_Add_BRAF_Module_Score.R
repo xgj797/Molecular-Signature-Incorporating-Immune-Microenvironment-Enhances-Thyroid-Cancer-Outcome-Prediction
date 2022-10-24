1e868f26ebb83e5a938ed04b34e4a534f194f096
mloberg16 <80286246+mloberg16@users.noreply.github.com>
2022-10-24 12:09:28 -0400

    Create Sample7_Add_BRAF_Module_Score.R

---
+++ Visium_Analysis/Sample_7/Sample7_Add_BRAF_Module_Score.R
@@ -0,0 +1,30 @@
+# Author: Matthew Aaron Loberg
+# Date: 22-0913
+# Goal: read in pre-processed Sample7 and add BRAF as a module score
+# Visiualize BRAF module score spatially
+
+# Load packages:
+library(tidyverse)
+library(Seurat)
+
+# Load processed Sample7
+Sample7 <- readRDS(file = "Data_in_Use/Sample7/Sample7_Clustered_0.3Res.rds")
+
+### Add the BRS Score
+## For this I will focus only on genes that are "UP" in BRAF lesions but NOT genes that are "UP" in RAS lesions
+## "BRAF Score"
+BRS_Genes <- as_tibble(read_csv(file = "Data_in_Use/BRS_Gene_List_71_Annotated.csv"))
+BRAF_Genes <- BRS_Genes %>% subset(Annotation == "BRAF")
+BRAF_Genes_List <- list(BRAF_Genes$BRS_Gene_list)
+
+
+# Add BRAF Gene Score to Sample7
+Sample7 <- AddModuleScore(object = Sample7,
+                          features = BRAF_Genes_List,
+                          name = "BRAF_Score")
+
+# Print BRAF Spatial Feature Plot
+BRAF_Spatial <- SpatialFeaturePlot(Sample7, features = c("BRAF_Score1"))
+ggsave("outputs/Sample7_ModuleScores/Sample7_BRAF_ModuleScore.png",
+       BRAF_Spatial,
+       width = 4, height = 5, dpi = 600)
