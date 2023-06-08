d4284a402c42695c70e0b8f3e77b37f5a908d4ff
mloberg16 <80286246+mloberg16@users.noreply.github.com>
2023-06-08 16:22:12 -0500

    Update Calculate_MAP_Score_in_TCGA.R

--- TCGA/Calculate_MAP_Score_in_TCGA.R
+++ TCGA/Calculate_MAP_Score_in_TCGA.R
@@ -1,6 +1,6 @@
 # Author: Matthew Aaron Loberg
 # Date: 23-0608 (date that script was cleaned up for the GitHub for our resubmission to Cell Genomics)
-# Script: Calculate_MAP_Score_in_TCGA.R
+# Script: Calculate_MAP_Score_in_TCGA_Validation_Cohort.R
 
 #### Notes on this script ####
 # This script will take the 549 genes that are in the molecular aggression and prediction (MAP) score and use them to calculate a MAP score for the samples in the TCGA papillary thyroid carcinoma (PTC) cohort
